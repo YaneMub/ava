76fd0a5a45b935442c03fcfa85a973072b2b83e5
Yane Mubilla <yanela.mubilla@gmail.com>
2026-04-13 22:48:53 -0300

    Añadiendo componente saludos

---
+++ src/data/saludos.js
@@ -0,0 +1,58 @@
+export const saludos = [
+    {
+        id: 1,
+        palabra: "hola",
+        categoria: "saludos",
+        video: ""
+    }
+
+    {
+        id: 2,
+        palabra: "chau",
+        categoria: "saludos",
+        video: ""
+    }
+    {
+        id: 3,
+        palabra: "si",
+        categoria: "saludos",
+        video: ""
+    }
+    {
+        id: 4,
+        palabra: "no",
+        categoria: "saludos",
+        video: ""
+    }
+    {
+        id: 5,
+        palabra: "bueno",
+        categoria: "saludos",
+        video: ""
+    }
+    {
+        id: 6,
+        palabra: "gracias",
+        categoria: "saludos",
+        video: ""
+    }
+    {
+        id: 7,
+        palabra: "de nada",
+        categoria: "saludos",
+        video: ""
+    }
+    {
+        id: 8,
+        palabra: "por favor",
+        categoria: "saludos",
+        video: ""
+    }
+    {
+        id: 9,
+        palabra: "perdón",
+        categoria: "saludos",
+        video: ""
+    }
+
+]
